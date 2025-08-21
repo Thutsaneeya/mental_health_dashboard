83e3ee493bc29fea6214e4ddf8ce3d3f60b41e40
Thutsaneeya <thutsaneeya@outlook.co.th>
2025-08-21 14:37:58 +0700

    add get_geojson function

--- loaders/load_data_main.py
+++ loaders/load_data_main.py
@@ -1,23 +1,21 @@
 # load_data_main.py
 # Load data for load_data.py
-# Return DataFrame
+# Return DataFrame and geojsone
 
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from data_preparation.load_data import (read_raw_data, data_info, clean_data)
-from configs.file_paths import filename  
+from data_preparation.load_data import read_raw_data, data_info, load_geojson
+from configs.file_paths import filename, url  
 
-def load_data_main():
-    df_raw = read_raw_data(filename)
-    #print("Loading...")
-    df_info = data_info(df_raw)
-    #print("Infomation...")
-    df_cleaned = clean_data(df_raw)
-    #print("Cleaning...")
-
-    return df_cleaned
-
+# Return DataFrame
 def get_data():
-    return load_data_main()
+    df_raw = read_raw_data(filename)
+    data_info(df_raw)
+
+    return df_raw
+
+# Return geojson
+def get_geojson():
+    return load_geojson(url)
 
