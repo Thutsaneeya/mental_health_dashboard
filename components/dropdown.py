8e5a0ed879aa2e1cdb4072d5d50ac1fcb62c005e
Thutsaneeya <thutsaneeya@outlook.co.th>
2025-08-21 14:35:06 +0700

    medified function

--- components/dropdown.py
+++ components/dropdown.py
@@ -7,12 +7,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.import_tools import st
 
-def show_selections(df):
-    # Create columns
-    #select_col1, select_col2 = st.columns(2)
-
-    # Select province
-    #with select_col1:
+def get_selections(df):
     selected_year = st.selectbox("ปี", sorted(df["ปี"].unique()), key = "year_select")
     #selected_province = st.selectbox("จังหวัด", sorted(df["จังหวัด"].unique()), key = "province_select")
 
@@ -21,7 +16,3 @@ def show_selections(df):
     #selected_disease = st.selectbox("โรค", sorted(df["ประเภทโรค"].unique()), key = "disease_select")
 
     return selected_year #selected_province, selected_disease
-
-# Get province, disease
-def get_selections(df):
-    return show_selections(df)
