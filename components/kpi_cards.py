8e7816be9f4fb338128d68aeb326a9e96ecc7b93
Thutsaneeya <thutsaneeya@outlook.co.th>
2025-08-22 16:01:11 +0700

    modified functions

--- components/kpi_cards.py
+++ components/kpi_cards.py
@@ -5,21 +5,23 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.import_tools import st
+from utils.filter_tools import format_metric
 
 # KPI card for total number of patients
 def overview_total_patients(df, year):
-    return st.metric(label = f"{year}", value = f"{df["จำนวนผู้ป่วย"].sum():,} ราย")
+    # format_metric
+    return st.metric(label = f"{year}", value = f"{format_metric(df['รวม'].iloc[0])}")
 
 # KPI card for missing province
 def overview_missing_province(df):
-    return st.metric(label = "ไม่ระบุจังหวัด", value = f"{df["จำนวนผู้ป่วย"].sum():,} ราย")
+    return st.metric(label = "ไม่ระบุจังหวัด", value = f"{format_metric(df['รวม'].iloc[0])}")
 
 # KPI for top province by number of patients
 def overview_top_patients(df):
     #  delta_color = "inverse"
-    return st.metric(label = " ", value = df["จังหวัด"], delta = f"{df["จำนวนผู้ป่วย"]:,} ราย")
+    return st.metric(label = df["จังหวัด"], value = f"{format_metric(df['รวม'])}")
 
 # KPI for rare province by number of patients
 def overview_min_patients(df):
-    return st.metric(label = " ", value = df["จังหวัด"], delta = f"{df["จำนวนผู้ป่วย"]:,} ราย")
+    return st.metric(label = df["จังหวัด"], value = f"{format_metric(df['รวม'])}")
 
