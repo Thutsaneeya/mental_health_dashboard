ff09e2b6f386d6c2879eb5982d8e95246c09b81d
Thutsaneeya <thutsaneeya@outlook.co.th>
2025-08-22 16:01:55 +0700

    add parameter: filename2

--- loaders/load_data_main.py
+++ loaders/load_data_main.py
@@ -6,11 +6,11 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from data_preparation.load_data import read_raw_data, data_info, load_geojson
-from configs.file_paths import filename, url  
+from configs.file_paths import filename2, url  
 
 # Return DataFrame
 def get_data():
-    df_raw = read_raw_data(filename)
+    df_raw = read_raw_data(filename2)
     data_info(df_raw)
 
     return df_raw
