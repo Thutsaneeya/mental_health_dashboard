237848e9392988d120dabd2c21c435c67fee4531
Thutsaneeya <thutsaneeya@outlook.co.th>
2025-08-23 22:34:15 +0700

    add function: get_top_disease

--- utils/filter_tools.py
+++ utils/filter_tools.py
@@ -4,7 +4,6 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from utils.import_tools import pd
 
 # Filter DataFrame by year
 def select_by_year(df, year):
@@ -34,9 +33,7 @@ def filter_province(df):
           df_m_province = df[df["จังหวัด"] == "ไม่ระบุจังหวัด"][["จังหวัด", "รวม"]]
           
           # Filter for data table
-          df_patients = df[df["จังหวัด"] != "รวม"][["จังหวัด", "รวม"]]
-
-          # For donut chart
+          df_patients = df[["จังหวัด", "รวม"]]
           
           # Get maximum and minimum rows of patients for KPI cards
           df_top_p = df_patients.loc[df_patients["รวม"].idxmax()]
@@ -51,53 +48,53 @@ def filter_province(df):
 # Reshape DataFrame 
 def reshape_disease_summary(df):
      try:
+          # Filter for heatmap
           # Total "รวม" rows
           df_totla_raw = df[df["จังหวัด"] == "รวม"]
+          # Drop columns
           df_total = df_totla_raw.drop(columns = ["จังหวัด", "รวม", "pro_code"])
+          # Melte DataFrame
           df_sum = df_total.melt(id_vars = "ปี", var_name = "โรค", value_name = "จำนวนผู้ป่วย")
 
           # With out "รวม" rows
           df_detail_raw = df[df["จังหวัด"] != "รวม"]
+          # Drop columns
           df_detail = df_detail_raw.drop(columns = ["รวม", "pro_code"])
+          # Melte DataFrame
           df_detail_melted = df_detail.melt(id_vars = ["ปี", "จังหวัด"], var_name = "โรค", value_name = "จำนวนผู้ป่วย")
+          # Get maximum number of patients by disease
           df_hotspot = (
                          df_detail_melted.groupby(["ปี", "โรค"])
                          .apply(lambda g: g.loc[g["จำนวนผู้ป่วย"].idxmax()]).reset_index(drop = True)
           )[["ปี", "โรค", "จังหวัด"]]
 
-          # Merge DataFrame for Heatmap
+          # Merge DataFrame for heatmap
           df_reshape = df_sum.merge(df_hotspot, on = ["ปี", "โรค"], how = "left")
 
-          # Filter for donut chart
-          # Top disease by year
-          df_top_disease = (
-                         df_detail_melted.groupby(["ปี", "โรค"])["จำนวนผู้ป่วย"]
-                         .sum()
-                         .reset_index()
-                         .sort_values(["ปี", "จำนวนผู้ป่วย"], ascending = [True, False])
-                         .groupby("ปี")
-                         .head(1)
-                         .reset_index(drop=True)
-          )
-          # Top province per disease
-          df_top_province = (
-                         df_detail_melted[df_detail_melted.set_index(["ปี", "โรค"]).index.isin(df_top_disease.set_index(["ปี", "โรค"]).index)]
-                         .groupby(["ปี", "โรค"])
-                         .apply(lambda g: g.loc[g["จำนวนผู้ป่วย"].idxmax()])
-                         .reset_index(drop=True)[["ปี", "โรค", "จังหวัด"]]
-          )
-
-          df_top = df_top_disease.merge(df_top_province, on = ["ปี", "โรค"])
-
-          return df_reshape, df_top
+          return df_reshape, df_detail_melted
 
      except Exception as e:
         print(f"เกิดข้อผิดพลาด: {e}")
         #return df
 
+# Filter top 3 disease by year
+def get_top_disease(df, year):
+          # Top 3 disease by year
+          df_disease_year = df[df["ปี"] == year]
+
+          df_top_disease = (
+                              df_disease_year.sort_values(["จำนวนผู้ป่วย"], ascending = False)
+                              # Get only the row with the highest patient count per disease
+                              .groupby("โรค").head(1)
+                              # Get top 3 diseases overall
+                              .sort_values("จำนวนผู้ป่วย", ascending = False).head(3)
+                              .reset_index(drop = True)
+          )
+
+          return df_top_disease.sort_values("จำนวนผู้ป่วย", ascending = False)
+
 # Sort DataFrame
 def sorted_data(df):
-     print("Sorted!......")
      # Sort value
      df = df[["จังหวัด", "รวม"]]
      #df_sort = (df.groupby("จังหวัด", as_index = False)["จำนวนผู้ป่วย"].sort_values(by = "จำนวนผู้ป่วย", ascending = False)).reset_index()
@@ -107,19 +104,15 @@ def sorted_data(df):
 # Format for KPI cards
 def format_metric(n):
      if n > 1000000:
-          print("1 ckecked!...")
           if (n % 1000000 == 0):
-               print("2 ckecked!...")
                return f"{n // 1000000} ล้าน"
           else:
                # round to 2 decimal
-               print("3 ckecked!...")
                var = round(n / 1000000, 2)
                var_str = f"{var:.2f}"
                
                # If the second decimal digit = 0, drop it for cleaner display
                if var_str[-1] == "0":
-                    print("4 ckecked!...")
                     var_str = f"{var:.1f}"
                
                return f"{var_str} ล้าน"
